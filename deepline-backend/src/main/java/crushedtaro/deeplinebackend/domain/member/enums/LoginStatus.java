56c84856578429827fe803836b00732c5228398a
crushed_taro <97sinsang@gmail.com>
2025-12-18 15:35:33 +0900

    feat : 인증 도메인 상태(Enum) 정의
    - 인증 도메인에서 사용하는 상태정의

---
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/member/enums/LoginStatus.java
@@ -0,0 +1,20 @@
+package crushedtaro.deeplinebackend.domain.member.enums;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import crushedtaro.deeplinebackend.domain.common.ResponseStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum LoginStatus implements ResponseStatus {
+  SIGNUP_SUCCESS(HttpStatus.CREATED, "SIGNUP_001", "회원가입이 성공적으로 완료되었습니다."),
+  SIGNUP_FALSE(HttpStatus.BAD_REQUEST, "SIGNUP_002", "회원가입에 실패하였습니다."),
+  ;
+
+  private final HttpStatus status;
+  private final String code;
+  private final String message;
+}
