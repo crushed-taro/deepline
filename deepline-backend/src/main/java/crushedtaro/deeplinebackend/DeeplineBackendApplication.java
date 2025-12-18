da0bfbc41ebdf9d513c9ae28547080632dc3c11d
crushed_taro <97sinsang@gmail.com>
2025-12-18 15:43:07 +0900

    init : Spring Boot 애플리케이션 엔트리포인트 추가

---
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/DeeplineBackendApplication.java
@@ -0,0 +1,12 @@
+package crushedtaro.deeplinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplineBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeeplineBackendApplication.class, args);
+  }
+}
