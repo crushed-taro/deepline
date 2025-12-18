772e8d2dc1865fc82b339b56455b06d0e0a8e071
crushed_taro <97sinsang@gmail.com>
2025-12-18 15:40:36 +0900

    feat : Spring Security 및 비밀번호 암호화 설정 추가
    - PasswordEncoder 설정, BCrypt
    - 인증/인가, 필터 체인, 접근 제어

---
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/infra/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package crushedtaro.deeplinebackend.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
+
+import crushedtaro.deeplinebackend.infra.jwt.JwtAccessDeniedHandler;
+import crushedtaro.deeplinebackend.infra.jwt.JwtAuthenticationEntryPoint;
+import crushedtaro.deeplinebackend.infra.jwt.JwtFilter;
+import crushedtaro.deeplinebackend.infra.jwt.TokenProvider;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+  private final TokenProvider tokenProvider;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .exceptionHandling(
+            exception -> {
+              exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+              exception.accessDeniedHandler(jwtAccessDeniedHandler);
+            })
+        .authorizeHttpRequests(
+            auth -> {
+              auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+              auth.requestMatchers("/").authenticated();
+              auth.requestMatchers("/api/v1/auth/**").permitAll();
+              auth.requestMatchers("/api/**").hasAnyRole("USER", "ADMIN");
+              auth.anyRequest().permitAll();
+            })
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .cors(cors -> {})
+        .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+}
