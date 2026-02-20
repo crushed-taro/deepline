259ef4dc36246cdf301faeb0ddf6b6936620ba55
crushed-taro <97sinsang@gmail.com>
2026-02-20 21:24:17 +0900

    feat(Spring) : from 정적 팩토리 메서드 추가
    - spotless 적용

--- deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/member/dto/MemberResponseDTO.java
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/member/dto/MemberResponseDTO.java
@@ -2,6 +2,8 @@ package crushedtaro.deeplinebackend.domain.member.dto;
 
 import java.time.LocalDateTime;
 
+import crushedtaro.deeplinebackend.domain.member.entity.Member;
+
 public record MemberResponseDTO(
     int memberCode,
     String memberId,
@@ -11,4 +13,23 @@ public record MemberResponseDTO(
     LocalDateTime createdAt,
     String deptName,
     String positionName,
-    String profileUrl) {}
+    String profileUrl) {
+
+  public static MemberResponseDTO from(Member member) {
+    String deptName =
+        (member.getDepartment() != null) ? member.getDepartment().getDeptName() : "미배정";
+    String positionName =
+        (member.getPosition() != null) ? member.getPosition().getPositionName() : "미배정";
+
+    return new MemberResponseDTO(
+        member.getMemberCode(),
+        member.getMemberId(),
+        member.getMemberName(),
+        member.getMemberEmail(),
+        member.getRemainVacation(),
+        member.getCreatedAt(),
+        deptName,
+        positionName,
+        member.getProfileUrl());
+  }
+}
