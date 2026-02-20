2edab5c7136fc931acf9e4f6d147b6095d1a6d7d
crushed-taro <97sinsang@gmail.com>
2026-02-20 21:50:46 +0900

    rafactor(Spring) : Spotless 다시 적용
    - spotless 적용

--- deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/service/ChatService.java
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/service/ChatService.java
@@ -3,14 +3,11 @@ package crushedtaro.deeplinebackend.domain.chat.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import crushedtaro.deeplinebackend.domain.chat.dto.ChatDTO;
 import crushedtaro.deeplinebackend.domain.chat.dto.ChatRoomResponse;
