0f475797bceca8347a81e97e39868f329efd5205
crushed-taro <97sinsang@gmail.com>
2026-02-28 15:20:41 +0900

    feat(React) : chatWidget 컴포넌트 추가, useEffect SSE 수정
    - prettier 적용

--- deepline-frontend/src/layouts/MainLayout.tsx
+++ deepline-frontend/src/layouts/MainLayout.tsx
@@ -1,52 +1,12 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "@/components/Header.tsx";
 import { AppSidebar } from "@/components/common/app-siderbar.tsx";
-import { toast } from "sonner";
-import { useEffect } from "react";
-import { EventSourcePolyfill } from "event-source-polyfill";
+import ChatWidget from "@/components/chat/ChatWidget.tsx";
+import { useSSE } from "@/hooks/notification/useSSE.ts";
 
 export default function MainLayout() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-    if (!token) return;
-
-    const eventSource = new EventSourcePolyfill("/api/v1/notifications/subscribe", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      heartbeatTimeout: 86400000,
-    });
-
-    eventSource.onopen = () => {
-      console.log("[SSE] Connected!");
-    };
-
-    eventSource.addEventListener("notification", (event: any) => {
-      const message = JSON.parse(event.data);
-      console.log("[SSE] Message Received:", message);
-
-      toast.success(message.content, {
-        action: {
-          label: "이동",
-          onClick: () => navigate(message.url),
-        },
-        duration: 5000,
-      });
-    });
-
-    eventSource.onerror = (err) => {
-      console.error("[SSE] Error:", err);
-      eventSource.close();
-    };
-
-    return () => {
-      eventSource.close();
-      console.log("[SSE] Connection Closed");
-    };
-  }, [navigate]);
+  useSSE();
 
   return (
     <SidebarProvider>
@@ -67,6 +27,7 @@ export default function MainLayout() {
             <Outlet />
           </main>
         </div>
+        <ChatWidget />
       </div>
     </SidebarProvider>
   );
