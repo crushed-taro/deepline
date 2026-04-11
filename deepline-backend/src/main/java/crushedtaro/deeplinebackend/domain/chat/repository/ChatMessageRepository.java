5625781d823b5c02ca43fc0320d988377578b4e5
crushed-taro <97sinsang@gmail.com>
2026-04-11 15:43:43 +0900

    feat(Spring) : 채팅 히스토리 조회 N+1 문제 개선
    - spotless 적용

--- deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/repository/ChatMessageRepository.java
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/repository/ChatMessageRepository.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import crushedtaro.deeplinebackend.domain.chat.entity.ChatMessage;
 import crushedtaro.deeplinebackend.domain.chat.entity.ChatRoom;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+  @EntityGraph(attributePaths = {"sender"})
   List<ChatMessage> findAllByChatRoomOrderByCreatedAtAsc(ChatRoom chatRoom);
 
+  @EntityGraph(attributePaths = {"sender"})
   Page<ChatMessage> findAllByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom, Pageable pageable);
 }
--- deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/repository/ChatRoomRepository.java
+++ deepline-backend/src/main/java/crushedtaro/deeplinebackend/domain/chat/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package crushedtaro.deeplinebackend.domain.chat.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import crushedtaro.deeplinebackend.domain.chat.entity.ChatRoom;
@@ -9,5 +10,6 @@ import crushedtaro.deeplinebackend.domain.chat.entity.ChatRoom;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
   Optional<ChatRoom> findByRoomId(String roomId);
 
+  @EntityGraph(attributePaths = {"member1", "member2"})
   Optional<ChatRoom> findByChatRoomCode(Long roomId);
 }
